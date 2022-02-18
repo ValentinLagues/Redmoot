656fbc0dd6b2acfc9b298ff6ed18159f6d022dcf
ValentinLagues <valentin.lagues@gmail.com>
2022-02-18 20:05:30 +0100

    appel API ok, reste le CSS du form, le zoom sur carousel, afficher la map, les tippys et le menu burger responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,29 @@
-const getCountriesName = async () => {
+const getCountries = async () => {
   try {
     const response = await axios.get(`https://restcountries.com/v3.1/all`);
-    const countriesName = response.data;
-    console.log(countriesName);
-    return countriesName;
+    return response.data;
   } catch (errors) {
     console.error(errors);
   }
 };
 
-getCountriesName();
+const createOptions = (countries) => {
+  const countriesList = document.getElementById("country-select");
+  {
+    countries.map((country) => {
+      // je créée l'option
+      const newOption = document.createElement("option");
+      newOption.text = country.name.common;
+      newOption.value = country.name.common;
+      // je la rajoute au select
+      countriesList.appendChild(newOption);
+    });
+  }
+};
+
+const selectCountries = async () => {
+  const countries = await getCountries();
+  createOptions(countries);
+};
+
+selectCountries();
